7652625dd58bd4fa3b4f19864620ed94b635a7e8
Raju <paladityaraj353@gmail.com>
2026-06-29 15:34:52 +0530

    chore: clean up starter frontend files

--- src/store/setupStore.ts
+++ src/store/setupStore.ts
@@ -6,6 +6,6 @@ interface SetupStore {
 }
 
 export const useSetupStore = create<SetupStore>((set) => ({
-  visits: 1,
+  visits: 0,
   incrementVisits: () => set((state) => ({ visits: state.visits + 1 })),
 }));
