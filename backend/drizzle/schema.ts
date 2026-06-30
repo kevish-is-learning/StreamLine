1133762cf79475eae45244dd9766987c19063911
aditisingh60 <aditisingh6062@gmail.com>
2026-07-01 00:44:27 +0530

    added user and created table

--- backend/drizzle/schema.ts
+++ backend/drizzle/schema.ts
@@ -1,8 +1,12 @@
-import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, timestamp, boolean,  } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('User', {
   id: uuid('id').primaryKey().defaultRandom(),
+  name: text('name').notNull(),
   email: text('email').notNull().unique(),
+  avatar: text('avatar'),
+  password: text('password').notNull(),
+  isCreator: boolean('isCreator').default(false),
   createdAt: timestamp('createdAt', { withTimezone: true })
     .notNull()
     .defaultNow(),
@@ -10,3 +14,16 @@ export const users = pgTable('User', {
     .notNull()
     .defaultNow(),
 });
+
+export const creator = pgTable('Creator', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  banner: text('banner').notNull(),
+  userId: uuid('userId').references(() => users.id).notNull(),
+  socialLinks: text('socialLinks').array(),
+  bio: text('bio').notNull(),
+  
+  
+  
+
+  
+})
